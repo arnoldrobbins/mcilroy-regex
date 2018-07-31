ec152f690d347895a589d8e23e5743a20ed84434
Michael Tesch <tesch1@gmail.com>
2018-07-31 12:37:54 +0200

    parallel fuzzing and test minimization

--- testfuzz.sh
+++ testfuzz.sh
@@ -6,14 +6,18 @@ set -e
 
 beginswith() { case $2 in "$1"*) true;; *) false;; esac; }
 
+#: ${AFL_CXX=afl-g++}
+: ${AFL_CXX=afl-clang++}
+: ${AFL_FUZZ=afl-fuzz}
+: ${AFL_TMIN=afl-tmin}
+
 # download and build afl if it's not already in the path
-AFL_CXX=afl-g++
-if ! [ command -v ${AFL_CXX} > /dev/null ]; then
+if ! command -v "${AFL_CXX}" > /dev/null ; then
     AFL_VERSION=2.52b
-    AFL_CXX=afl-${AFL_VERSION}/afl-g++
-    AFL_CXX=afl-${AFL_VERSION}/afl-clang++
-    AFL_FUZZ=afl-${AFL_VERSION}/afl-fuzz
-    if ! [ -x ${AFL_CXX} ]; then
+    AFL_CXX=./afl-${AFL_VERSION}/${AFL_CXX}
+    AFL_FUZZ=./afl-${AFL_VERSION}/${AFL_FUZZ}
+    AFL_TMIN=./afl-${AFL_VERSION}/${AFL_TMIN}
+    if ! [ -x "${AFL_CXX}" ]; then
         # doesn't exist, download and build it
         if ! [ -f afl-${AFL_VERSION}.tgz ]; then
             wget http://lcamtuf.coredump.cx/afl/releases/afl-${AFL_VERSION}.tgz
@@ -21,10 +25,6 @@ if ! [ command -v ${AFL_CXX} > /dev/null ]; then
         tar xzvf afl-${AFL_VERSION}.tgz
         cd afl-${AFL_VERSION} && make && cd ..
     fi
-else
-    AFL_CXX=afl-clang++
-    AFL_CXX=afl-g++
-    AFL_FUZZ=afl-fuzz
 fi
 
 # rebuild testre
@@ -34,10 +34,25 @@ CXX="$AFL_CXX" make testre
 # chop up the data file into one-liners
 mkdir -p fuzz-data
 lineno=0
-while read line || [[ -n "$line" ]]; do
+while read line || [ -n "$line" ]; do
     lineno=$(( lineno = lineno + 1 ))
     if [ "x$line" = "x" ] || beginswith "#" "$line"; then continue ; fi
     echo "$line" > fuzz-data/${lineno}.dat
 done < testre.dat
-pwd
-$AFL_FUZZ -i fuzz-data -o fuzz-out ./testre
+
+# go ahead and fuzz.  crash inputs in fuzz-out/crashes/id*
+echo "to run additional parallel slave fuzzers in other terminals:"
+echo "cd \"$(pwd)\" && $AFL_FUZZ -i fuzz-data -o fuzz-out -S fuzzer01 ./testre"
+echo "cd \"$(pwd)\" && $AFL_FUZZ -i fuzz-data -o fuzz-out -S fuzzer02 ./testre"
+echo "...and so on..."
+
+set +e
+"$AFL_FUZZ" -i fuzz-data -o fuzz-out -M fuzzer00 ./testre
+
+# when finished/bored, minimize the crash cases:
+echo "minimizing crash cases..."
+mkdir -p fuzz-out-min
+set -e
+for f in fuzz-out/fuzzer*/crashes/id*; do
+    "$AFL_TMIN" -i "$f" -o "fuzz-out-min/$(basename "$f").min" -- ./testre
+done
