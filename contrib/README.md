513585bf076fd66f8ef4d31f3f954b95f1eb70e3
Michael Tesch <tesch1@gmail.com>
2018-08-01 08:37:28 +0200

    moved to contrib/ dir, added contrib/README.md

---
+++ contrib/README.md
@@ -0,0 +1,12 @@
+## Contributions
+
+File(s) | Notes | Author
+--- | --- | ---
+[testfuzz.sh](./blob/master/contrib/testfuzz.sh) | A script to fuzz the ./testre program. | Michael Tesch, tesch1@gmail.com
+
+### testfuzz.sh
+
+This script fuzzes the ./testre program using the handy [AFL (american
+fuzzy lop)](http://lcamtuf.coredump.cx/afl/).  If AFL isn't available
+it will be downloaded and built locally.
+
--- contrib/testfuzz.sh
+++ contrib/testfuzz.sh
@@ -10,6 +10,9 @@ beginswith() { case $2 in "$1"*) true;; *) false;; esac; }
 : ${AFL_CXX=afl-clang++}
 : ${AFL_FUZZ=afl-fuzz}
 : ${AFL_TMIN=afl-tmin}
+: ${AFL_VERSION=2.52b}
+: ${FUZZ_IN=fuzz-data}
+: ${FUZZ_OUT=fuzz-out}
 : ${TESTRE_FLAGS=}
 : ${DO_FUZZ=yes}
 : ${DO_TMIN=yes}
@@ -17,22 +20,32 @@ beginswith() { case $2 in "$1"*) true;; *) false;; esac; }
 while [ $# -gt 0 ]; do
     key="$1"
     case $key in
-        tmin) DO_FUZZ=no ;;
         fuzz) DO_TMIN=no ;;
+        tmin) DO_FUZZ=no ;;
+        -c) FUZZ_IN="-" ;;
         -n) TESTRE_FLAGS="${TESTRE_FLAGS} -n" ;;
         -v) TESTRE_FLAGS="${TESTRE_FLAGS} -v" ;;
         *)
             echo "unrecognized arg: $key"
+            echo "usage: $0 [fuzz|tmin] [-c] [-n] [-v]"
+            echo "  fuzz  - only run the fuzzer, dont minimize test cases"
+            echo "  tmin  - only minimize existing crash cases, dont fuzz"
+            echo "  -c    - continue an existing, interrupted fuzz run"
+            echo "  -n|-v - pass either/both of these flags to ./testre"
+            echo " (see testfuzz.sh for configurable env variables)"
             exit 1
             ;;
     esac
     shift
 done
 
+# make sure we're in the base mcilroy-regex directory
+if [ ! -f testre.cpp ] && [ -f ../testre.cpp ]; then
+    cd ..
+fi
 
 # download and build afl if it's not already in the path
 if ! command -v "${AFL_CXX}" > /dev/null ; then
-    AFL_VERSION=2.52b
     AFL_CXX=./afl-${AFL_VERSION}/${AFL_CXX}
     AFL_FUZZ=./afl-${AFL_VERSION}/${AFL_FUZZ}
     AFL_TMIN=./afl-${AFL_VERSION}/${AFL_TMIN}
@@ -62,19 +75,20 @@ if [ ${DO_FUZZ} = yes ]; then
 
     # go ahead and fuzz.  crash inputs in fuzz-out/crashes/id*
     echo "to run additional parallel slave fuzzers in other terminals:"
-    echo "cd \"$(pwd)\" && $AFL_FUZZ -i fuzz-data -o fuzz-out -S fuzzer01 ./testre ${TESTRE_FLAGS}"
-    echo "cd \"$(pwd)\" && $AFL_FUZZ -i fuzz-data -o fuzz-out -S fuzzer02 ./testre ${TESTRE_FLAGS}"
+    echo "cd \"$(pwd)\" && $AFL_FUZZ -i ${FUZZ_IN} -o ${FUZZ_OUT} -S fuzzer01 ./testre ${TESTRE_FLAGS}"
+    echo "cd \"$(pwd)\" && $AFL_FUZZ -i ${FUZZ_IN} -o ${FUZZ_OUT} -S fuzzer02 ./testre ${TESTRE_FLAGS}"
     echo "...and so on..."
 
     set +e
-    "$AFL_FUZZ" -i fuzz-data -o fuzz-out -M fuzzer00 ./testre ${TESTRE_FLAGS}
+    "$AFL_FUZZ" -i "${FUZZ_IN}" -o "${FUZZ_OUT}" -M fuzzer00 ./testre ${TESTRE_FLAGS}
+    set -e
 fi
 
 if [ ${DO_TMIN} = yes ]; then
     # when finished/bored, minimize the crash cases:
     echo "minimizing crash cases..."
     mkdir -p fuzz-out-min
-    for f in fuzz-out/fuzzer*/crashes/id*; do
+    for f in "${FUZZ_OUT}"/fuzzer*/crashes/id* ; do
         fmin="fuzz-out-min/$(basename "$f").min"
         # make sure ./testre still crashes on this input
         set +e
